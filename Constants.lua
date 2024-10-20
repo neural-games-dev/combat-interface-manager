b409ed68af5da1763ce157399fb95952da168fa2
Metal Gabe <anewlevelmp@gmail.com>
2024-10-19 20:00:20 -0700

    chore(main): add starting constants values;

---
+++ Constants.lua
@@ -0,0 +1,19 @@
+--## ===============================================================================================
+--## ALL REQUIRED IMPORTS
+--## ===============================================================================================
+-- Libs / Packages
+local CombatInterfaceManager = LibStub('AceAddon-3.0'):GetAddon('CombatInterfaceManager');
+
+--## ===============================================================================================
+--## INTERNAL VARS & SET UP
+--## ===============================================================================================
+local chalk = CombatInterfaceManager:GetModule('Chalk');
+
+--## ==========================================================================
+--## DEFINING THE GLOBAL CONSTANTS TABLE TO BE USED THROUGHOUT THE ADDON
+--## ==========================================================================
+-- TODO **[G]** :: Can/should I attach this as part of the main addon/self as a property?
+CIM_Constants = {
+   addOnName = chalk:ace('Combat Interface Manager'),
+   addOnNameQuoted = chalk:ace('"Combat Interface Manager"'),
+}
